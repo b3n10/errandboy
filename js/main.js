d9156bffd333265c4d6b7a52685e9fb4030f6f7f
b3n10 <francisben.lleve@gmail.com>
2019-10-28 13:44:35 +0800

    w/ additional stop over

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var tot_cost = document.getElementById('total_cost');
 var stop = document.getElementById('stop');
 var sp = document.getElementById('stop_plus');
 var sm = document.getElementById('stop_minus');
+var st_cost = document.getElementById('stop_cost');
 
 var result = document.getElementsByClassName('result')[0];
 
@@ -15,33 +16,41 @@ var calculate = function() {
 
     d = Number(dist.value);
 
+    var remkm = d - 3;
+    var remcost = remkm * 15;
+    var total = remcost + 55;
+
+    s = stop.value * 20;
+    t = Number(s) + Number(total.toFixed(2));
+
     if (d) {
         if (d <= 3) {
-            rem_dist.innerHTML = "0km: ";
             rem_cost.innerHTML = "0";
-            tot_cost.innerHTML = "55php";
+            stop_cost.innerHTML = "(" + stop.value + "x20) " + s;
+            tot_cost.innerHTML = t + "php";
         } else {
-            var firstkm = d - 3;
-            rem_dist.innerHTML = firstkm.toFixed(1) + "km: ";
 
-            var remkm = firstkm * 15;
-            rem_cost.innerHTML = remkm.toFixed(2);
-
-            var total = remkm + 55;
-            tot_cost.innerHTML = total.toFixed(2) + "php";
+            stop_cost.innerHTML = "(" + stop.value + "x20) " + s;
+            rem_cost.innerHTML = "(" + remkm.toFixed(1)  + "x15) " + remcost.toFixed(2);
+            tot_cost.innerHTML = t + "php";
         }
     }
 };
 
 var stop_over = function() {
     var s = Number(stop.value);
+    var div_stop = document.getElementsByClassName('stop_text')[0];
 
     if (this === sp) {
         stop.value = s + 1;
+        div_stop.style.display = 'block';
     } else {
-        if (s != 0)
+        if (Number(stop.value) != 0) {
             stop.value = s - 1;
+            div_stop.style.display = 'none';
+        }
     }
+    calculate();
 };
 
 dist.addEventListener('input', calculate)
